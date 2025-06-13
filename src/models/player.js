c21443cacddba2d7f8aa5cecad9db9296734f745
PhuuQuiii <nguyenphuqui170304@gmail.com>
2025-06-14 04:44:06 +0700

    update player_uid

--- src/models/player.js
+++ src/models/player.js
@@ -1,56 +1,60 @@
-const { DataTypes } = require('sequelize');
-const sequelize = require('../config/database');
-const PlayerStats = require('./player_stats');
-const PlayerRelationships = require('./player_relationships');
+const { DataTypes } = require("sequelize");
+const sequelize = require("../config/database");
+const PlayerStats = require("./player_stats");
+const PlayerRelationships = require("./player_relationships");
 
-const Player = sequelize.define('player', {
+const Player = sequelize.define(
+  "player",
+  {
     player_id: {
-        type: DataTypes.BIGINT,
-        primaryKey: true,
-        autoIncrement: true
+      type: DataTypes.BIGINT,
+      primaryKey: true,
+      autoIncrement: true,
     },
     player_uid: {
-        type: DataTypes.STRING(36)
+      type: DataTypes.STRING(255),
     },
     player_stats_id: {
-        type: DataTypes.BIGINT,
-        references: {
-            model: PlayerStats,
-            key: 'player_stats_id'
-        }
+      type: DataTypes.BIGINT,
+      references: {
+        model: PlayerStats,
+        key: "player_stats_id",
+      },
     },
     player_relation_id: {
-        type: DataTypes.BIGINT,
-        references: {
-            model: PlayerRelationships,
-            key: 'player_relationship_id'
-        }
+      type: DataTypes.BIGINT,
+      references: {
+        model: PlayerRelationships,
+        key: "player_relationship_id",
+      },
     },
     name: {
-        type: DataTypes.STRING(50)
+      type: DataTypes.STRING(50),
     },
     gender: {
-        type: DataTypes.ENUM('male', 'female', 'other')
-    }
-}, {
-    timestamps: false
-});
+      type: DataTypes.ENUM("male", "female", "other"),
+    },
+  },
+  {
+    timestamps: false,
+  }
+);
 
 // Define relationships
 Player.belongsTo(PlayerStats, {
-    foreignKey: 'player_stats_id'
+  foreignKey: "player_stats_id",
 });
 
 Player.belongsTo(PlayerRelationships, {
-    foreignKey: 'player_relation_id'
+  foreignKey: "player_relation_id",
 });
 
 PlayerStats.hasOne(Player, {
-    foreignKey: 'player_stats_id'
+  foreignKey: "player_stats_id",
 });
 
 PlayerRelationships.hasOne(Player, {
-    foreignKey: 'player_relation_id'
+  foreignKey: "player_relation_id",
 });
 
 module.exports = Player;
